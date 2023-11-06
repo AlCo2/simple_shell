5f86e7934c628f04cc597540b8f9f99c44cb29d2
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-11-06 01:19:16 +0100

    change name of main

--- shell.c
+++ shell.c
@@ -5,23 +5,22 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
 	int status;
 	pid_t pid;
-	while(1){
+	while(1)
+	{
 		printf("$ ");
 		nread = getline(&line, &len, stdin);
 		line[nread-1] = '\0';
 		char* token = strtok(line," ");
-		if(fork()==0)
+		if(fork() == 0)
 			execve(token, NULL, NULL);
-
 		wait(&status);
-
 	}
 	return 0;
 }
