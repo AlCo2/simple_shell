7720521a67518be3856d2a8866c28d8338882408
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-11-06 02:08:23 +0100

    add support for arguments

--- shell.c
+++ shell.c
@@ -11,15 +11,40 @@ int main(int argc, char **argv)
 	size_t len = 0;
 	ssize_t nread;
 	int status;
-	pid_t pid;
+	char *token;
+	char **av = NULL;
 	while(1)
 	{
-		printf("$ ");
+		printf("\n$ ");
 		nread = getline(&line, &len, stdin);
 		line[nread-1] = '\0';
-		char* token = strtok(line," ");
+		token = strtok(line, " ");
+		size_t av_count = 0;
+		while(token!=NULL){
+			av_count++;
+			av = (char**)realloc(av, av_count * sizeof(char*));
+			if(av==NULL){
+				perror("out of memory");
+				return 1;
+			}
+			av[av_count-1] = malloc(strlen(token)+1);
+			if(av[av_count-1]==NULL){
+				perror("out of memory");
+				return 1;
+			}
+			strcpy(av[av_count-1], token);
+			token = strtok(NULL, " ");
+		}
+		free(token);
 		if(fork() == 0)
-			execve(token, NULL, NULL);
+		{
+			if(execve(av[0], av, NULL) == -1){
+				perror("Error");
+			}
+		}
+		for(size_t i=0;i<av_count;i++){
+			free(av[i]);
+		}
 		wait(&status);
 	}
 	return 0;
