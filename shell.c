619f8f46b1175ea3d2984c0dbfbd7c76429da60f
AlCo2 <abdou.lahboub.wwe@gmail.com>
2023-11-06 17:22:14 +0100

    handle cmd from PATH

--- shell.c
+++ shell.c
@@ -6,6 +6,51 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+
+char *check_path(char *token,char **PATHS)
+{
+	struct stat st;
+	int i=0;
+	while (PATHS[i])
+	{
+		char *temp = malloc(strlen(PATHS[i])+strlen(token)+2);
+		strcpy(temp, PATHS[i]);
+		strcat(temp, "/");
+		strcat(temp, token);
+		if(stat(temp, &st) == 0)
+		{
+			return (temp);
+		}
+		free(temp);
+		i++;
+	}
+	return NULL;
+}
+
+char **create_path_list(){
+	char *paths = getenv("PATH");
+	char **list = NULL;
+	char *token;
+        token = strtok(paths, ":");
+        int i = 0;
+        while(token){
+                i++;
+                list = (char**)realloc(list,i*sizeof(char*));
+                list[i-1] = malloc(strlen(token)+1);
+                strcpy(list[i-1], token);
+		token = strtok(NULL, ":");
+        }
+	return list;
+}
+
+void executeCMD(char **av){
+	if (fork() == 0)
+	{
+		if (execve(av[0], av, NULL) == -1)
+			perror("Error");
+	}
+}
+
 int main()
 {
 	char *line = NULL;
@@ -14,22 +59,20 @@ int main()
 	int status;
 	char *token = NULL;
 	char **av = NULL;
+	char **PATHS = create_path_list();	
 	struct stat st;
 
 	while (1)
 	{
 		size_t av_count = 0;
 		size_t i;
+		char* result = NULL;
 
 		printf("\n$ ");
 		nread = getline(&line, &len, stdin);
 		line[nread - 1] = '\0';
 		token = strtok(line, " ");
-		if (stat(token, &st) != 0)
-		{
-			printf("%s: 1: not found\n", token);
-			continue;
-		}
+		result = check_path(token, PATHS);
 		while (token != NULL)
 		{
 			av_count++;
@@ -49,11 +92,13 @@ int main()
 			token = strtok(NULL, " ");
 		}
 		free(token);
-		if (fork() == 0)
-		{
-			if (execve(av[0], av, NULL) == -1)
-				perror("Error");
+		if(result){
+			if(fork()==0){
+				execve(result, av, NULL);
+			}
 		}
+		else
+			executeCMD(av);
 		for (i = 0; i < av_count; i++)
 		{
 			free(av[i]);
